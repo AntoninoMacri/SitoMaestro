891542421ecaa035c420caa63288c8ba570c372a
Antonino Macrì <antonino.mac93@gmail.com>
2025-01-31 15:32:42 +0100

    icona x a burgher menù aperto

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -78,8 +78,26 @@
     <!-- Mobile menu icon -->
     <div class="md:hidden">
       <button @click="open = !open" class="text-white">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <!-- Se il menu è aperto, mostra la X, altrimenti l'hamburger -->
+        <svg
+          v-if="!open"
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+        <svg
+          v-if="open"
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
         </svg>
       </button>
     </div>
