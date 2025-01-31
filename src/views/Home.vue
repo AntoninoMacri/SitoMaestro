c2200e7ca1fde34f89aaf4925f81d07126671bad
Antonino Macrì <antonino.mac93@gmail.com>
2025-01-31 14:07:21 +0100

    allineato il testo in mobile e evitato che esca da dx rispetto all'immagine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="bg-gray-100 min-h-screen">
     <!-- Sezione Benvenuto -->
-    <section class="max-w-7xl mx-auto p-8 flex flex-col md:flex-row justify-between items-start space-x-8">
+    <section
+      class="max-w-7xl mx-auto p-8 flex flex-col md:flex-row justify-between items-center md:items-start gap-y-6 md:gap-x-8"
+    >
       <!-- Immagine sopra il testo su mobile -->
-      <div class="w-full md:w-1/3 mb-6 md:mb-0">
+      <div class="w-full md:w-1/3">
         <img
           src="../assets/SifuLuca-GranMasterCheung.jpeg"
           alt="Immagine Benvenuto"
           class="w-full rounded-lg shadow-lg"
         />
       </div>
+
       <!-- Contenuto Testuale -->
-      <div class="w-full md:w-2/3">
+      <div class="w-full md:w-2/3 text-left">
         <h1 class="text-3xl font-bold text-gray-800 mb-4">Benvenuti nella mia scuola di Wing Chun Tradizionale</h1>
 
         <div class="text-lg text-gray-700 space-y-6">
@@ -19,12 +22,10 @@
             Mi chiamo Luca Campiglio e sono allievo del GM William Cheung. Dal 1994 inizio a praticare il Wing Chun e
             dal 1997 pratico ed insegno il Wing Chun Tradizionale presso le mie scuole di Legnago, Montagnana e Zevio.
           </p>
-
           <p>
             Ai miei corsi sono benvenute tutte le persone che desiderano avvicinarsi a questa fantastica arte marziale
             inventata da una “monaca”.
           </p>
-
           <p>
             Sono sempre attento a proporre ai miei allievi varie strategie di allenamento organizzando anche Stage
             nazionali e internazionali.
