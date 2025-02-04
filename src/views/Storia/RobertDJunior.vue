8977c7be60c9aa7338980d40509f1a151047cc25
Antonino Macrì <antonino.mac93@gmail.com>
2025-02-04 16:17:21 +0100

    Pagine storia

--- src/views/Storia/RobertDJunior.vue
+++ src/views/Storia/RobertDJunior.vue
@@ -1,100 +1,134 @@
 <template>
-  <div class="bg-gray-100 min-h-screen flex flex-col">
-    <!-- Sezione Benvenuto -->
-    <section class="max-w-7xl mx-auto p-8 flex flex-col md:flex-row justify-between items-start gap-y-6 md:gap-x-8">
-      <!-- Contenuto Testuale e News (a sinistra su desktop, sopra su mobile) -->
-      <div class="w-full md:w-2/3 flex flex-col gap-8">
-        <!-- Testo -->
-        <div class="text-left">
-          <div class="text-lg text-gray-700 space-y-6">
-            <h1 class="text-red-600 font-bold text-5xl">Robert Downey Jr.</h1>
-
-            <h2 class="text-red-600 font-bold text-3xl">IronMan pratica il winchun Stile Cheung</h2>
-
-            <p class="text-black font-bold text-2xl">LEGNAGO (Verona)</p>
-            <p><strong>Dove:</strong> Via Leopardi, 3 – Scuole Elementari G. Cotta</p>
-            <p>
-              <strong>Quando:</strong> Martedì e Giovedì dalle ore 19.00 alle ore 20.00 – Inizio del corso Martedì 08
-              Ottobre 2024
-            </p>
-
-            <!-- Sezione "A chi mi rivolgo" -->
-            <p class="text-black font-bold text-2xl">BORGO SAN MARCO – MONTAGNANA (Padova)</p>
-            <p><strong>Dove:</strong> Scuole Elementari via Arzarin, 2</p>
-            <p>
-              <strong>Quando:</strong> Lunedì dalle ore 18.45 alle ore 19.45 – Inizio del corso Lunedì 07 Ottobre 2024
-            </p>
-
-            <!-- Sezione "Perchè i miei corsi" -->
-            <p class="text-black font-bold text-2xl">ZEVIO (Verona)</p>
-            <p><strong>Dove:</strong> Via Altichiero, 21 – Scuole Medie</p>
-            <p>
-              <strong>Quando:</strong> Venerdì dalle ore 18.30 alle ore 19.30 – Inizio del corso Venerdì 04 Ottobre 2024
-            </p>
-
-            <h2 class="text-red-600 font-bold text-3xl">CORSI WING CHUN ADVANCED E C.T.D.</h2>
-            <p class="text-black font-bold text-2xl">LEGNAGO (Verona)</p>
-            <p><strong>Dove:</strong> Via Leopardi, 3 – Scuole Elementari G. Cotta</p>
-            <p><strong>Quando:</strong> Sabato dalle ore 16.00 alle ore 17.30</p>
-
-            <!-- <h2 class="text-red-600 font-bold text-3xl">ULTERIORI INFO</h2>
-            <p>Per qualsiasi ulteriore infromazione riguardo i corsi contattatemi direttamente ai miei contatti:</p>
-            <p><strong>Cel. /WhatsApp</strong> 3358014379</p>
-            <p><strong>e-mail:</strong> cm.lucky71@gmail.com</p> -->
-          </div>
-        </div>
-      </div>
-
-      <!-- Video -->
-      <div class="w-full md:w-2/3">
-        <!-- Immagine -->
-        <img
-          :src="imageSrc"
-          alt="Caratteri cinesi di Wing Chun"
-          class="w-full rounded-lg shadow-lg cursor-pointer transition-transform hover:scale-105 md:max-h-90 object-cover mb-4"
-          @click="openLightbox"
-        />
-
-        <!-- Video -->
-        <video :src="videoSrc" class="w-full rounded-lg shadow-lg" controls loop>
-          Il tuo browser non supporta il tag video.
-        </video>
-
-        <!-- Nota sotto il video -->
-        <p class="text-center text-gray-600 mt-4">CORSI-2023-24</p>
-      </div>
-    </section>
-
-    <!-- Lightbox -->
-    <div
-      v-if="isLightboxOpen"
-      class="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center"
-      @click="closeLightbox"
+  <div class="max-w-4xl mx-auto p-6 text-gray-700 space-y-6">
+    <!-- Titolo principale -->
+    <h1 class="text-red-600 font-bold text-5xl">Robert Downey Jr.</h1>
+    <h2 class="text-red-600 font-bold text-3xl">IronMan pratica il winchun Stile Cheung</h2>
+    <p>
+      Nonostante il titolo scherzoso l'argomento tratto sarà molto serio infatti qui tratteremo il lato più umano del
+      percorso di un praticante di Wing Chun (per quanto famoso come Robert Downey Jr).
+    </p>
+    <p>
+      Qui riportiamo un video dedicato al maestro Eric Oram che con il suo insegnamento ha permesso il percorso di
+      riabilitazione e crescita di Robert Downey JR. e ha portato anche a noi i suoi insegnamenti tramite Stage regolari
+      nelle nostre sedi. Inoltre forniamo la traduzione dell'articolo "il Wing Chun ha aiutato Robert Downey Jr. a
+      combattere la dipendenza" di Vice.com
+    </p>
+    <iframe
+      class="w-full h-full min-h-screen rounded-lg shadow-lg"
+      src="https://www.youtube.com/embed/ZCFF7yOYK3I"
+      frameborder="0"
+      allowfullscreen
     >
-      <img :src="imageSrc" alt="Immagine Lightbox" class="max-w-full max-h-full rounded-lg shadow-lg" />
-    </div>
+    </iframe>
+
+    <p class="text-center text-gray-600 mt-4">
+      video dedicato al maetro Eric Oram sia con Robert Downey JR sia in visita presso le nostre palestre.
+    </p>
+    <p class="text-black font-bold text-2xl">Alcune storie partono dal basso...</p>
+    <p>
+      Ci sono una miriade di modi in cui le arti marziali possono migliorare la vita di una persona. Per il guerriero
+      del fine settimana, può essere un nuovo hobby divertente, insegnare alcuni trucchi interessanti e presentarti a
+      nuovi amici. Per lo studente più dedicato, può darti uno scopo, insegnarti la disciplina, aiutarti a creare una
+      nuova famiglia e forse anche portare a una nuova carriera o chiamata. Per alcune persone, però, intraprendere
+      un'arte marziale non è solo un'esperienza che cambia la vita; È un salvataggio di vite umane.
+    </p>
+
+    <p>
+      Robert Downey Jr., l'attore amato ma problematico, ha iniziato a sperimentare droghe da bambino, fumando spinelli
+      con il permesso di suo padre prima di compiere 10 anni, e il suo uso e la sua scelta di sostanze illecite sono
+      discesi rapidamente da lì. Ha trascorso la maggior parte dei due decenni successivi lottando contro la dipendenza,
+      accumulando arresti e violazioni della libertà vigilata – e una condanna a 180 giorni di carcere – a causa del suo
+      dilagante abuso di droghe e del suo comportamento. Aveva provato quasi tutto, compresi diversi tentativi di
+      riabilitazione, quando si era rivolto al Wing Chun come suo potenziale potere superiore.
+    </p>
+    <p>
+      Credendo che l'arte del colpire e del lottare, meglio conosciuta a Hollywood come l'arte marziale che Bruce Lee
+      praticava prima di sviluppare il Jeet Kune Do, potesse aiutarlo a dargli la routine, la concentrazione e la
+      filosofia che potevano tenerlo sul carro, Downey si rivolse al fondatore della LA Wing Chun Academy Eric Oram per
+      prendere lezioni.
+    </p>
+
+    <!-- Sezione "Perchè i miei corsi" Il vero maestro guarda negli occhi il proprio allievo e l'allievo deve rispondere senza dire una parola -->
+    <p class="text-black font-bold text-2xl">L'autentica convinzione è la base per costruire</p>
+    <p>
+      All'inizio Oram era dubbioso sull'impegno del famigerato attore. "Quando è venuto da me per la prima volta, le
+      compagnie di assicurazione non lo legavano per i film; non riusciva a ottenere ruoli", ha detto Oram a Men's
+      Journal nel 2011. "Gli ho detto che se non si fosse presentato a una lezione, gli avrei tagliato le dita dei piedi
+      e gliele avrei restituite. Un giorno non si presentò, e io gli dissi addio. Poi un paio di produttori mi hanno
+      chiamato e hanno garantito per lui, dicendo: 'Era con noi in una riunione; Non aveva un telefono. È colpa nostra.
+      Non tagliargli la testa'. Da allora si è impegnato e ha dato una svolta alla sua vita".
+    </p>
+    <p class="text-black font-bold text-2xl">Il lavoro interiore porta a risultati già nel breve periodo</p>
+
+    <p>
+      A un anno dall'inizio della sua formazione, Downey ha parlato dell'incredibile cambiamento che la sua formazione
+      ha apportato alla sua vita durante la sua apparizione su Oprah. "Martial è appena stato... Non posso nemmeno dire
+      quanto abbia influito sulla mia capacità di rimanere in buona salute e concentrata", ha detto la star nello show.
+      "È una pratica spirituale. Mi ha radicato e il suo scopo principale è quello di promuovere un senso di guerriero
+      spirituale e di rispettare la propria società e di essere pronti a difendere se stessi e la propria società, se
+      necessario".
+    </p>
+    <p class="text-black font-bold text-2xl">L'entusiasmo di Robert Downey Jr</p>
+
+    <p>
+      Come ogni nuovo convertito entusiasta, ha cercato di reclutare nuove persone per la causa, dicendo a Oprah che
+      avrebbe dovuto provarci. "Penso che faresti rock perché sei formidabile", ha insistito Downey. Dopo tre anni di
+      allenamento, proprio mentre stava per fare il test per la sua fascia blu medio, Downey ha parlato del suo continuo
+      amore per lo sport al Late Show With David Letterman. "Sono profondamente impegnato nel Wing Chun", ha detto al
+      conduttore, prima di spiegare e dimostrare vertiginosamente il Bil Gee (alias Biu Gee), l'arte di spingere le
+      dita.
+    </p>
+    <p>
+      Quando Letterman gli chiese se Bil Gee fosse l'elemento base del Wing Chun, Downey ammise in qualche modo
+      colpevolmente che c'era dell'altro. "No, e potrei mettermi nei guai perché se Sifu [Oram] sta guardando stasera,
+      la verità è che il Wing Chun è pensato per essere una forma d'arte molto aggraziata", ha detto, aggiungendo "Ho
+      bisogno di approfondire gli aspetti meditativi". Il suo Sifu ovviamente perdonò il caso dell'attore di "febbre da
+      cintura marrone", come Downey in seguito chiamò nell'apparizione, perché la coppia lavora ancora insieme fino ad
+      oggi. E la sobrietà non è l'unica cosa che la star ha guadagnato da questa esperienza.
+    </p>
+    <p class="text-black font-bold text-2xl">La carriera spicca il volo che lo porterà al tanto amato Ironman</p>
+    <p>
+      "L'obiettivo di Robert è notte e giorno da quando abbiamo iniziato", ha detto Oram a Men's Fitness. "Questa sua
+      mente da fucile lo ha incanalato in un unico punto di messa a fuoco e lo ha trasformato in un laser. L'allenamento
+      lo richiede. Durante uno scambio, se il tuo cervello è da qualche altra parte, ti prenderò. Se la mente si
+      allontana, becca. Fermarsi per darsi una pacca sulla spalla? Stai pensando alle tue tasse? Fatto." Man mano che
+      Downey assumeva ruoli più orientati all'azione, sia come protagonista nei film di Sherlock Holmes di Guy Ritchie
+      che come Iron Man nell'universo Marvel, ha iniziato ad aggiungere alcune delle sue abilità di Wing Chun alle sue
+      acrobazie. "La coreografia dei combattimenti cinematografici ha le sue esigenze", ha detto Oram. "È
+      concentrazione, controllo, tempismo e molta ripetizione. Si tratta di ricordare dove ci si trova in ogni fase del
+      percorso nel raccontare una storia e tuttavia di giocarla come se stesse accadendo per la prima volta dal vivo.
+      C'è un'arte in questo, e Robert lavora molto, molto duramente in questo processo. Ho combattuto due volte per lui
+      per cose minori, come i colpi di raccolta, ma Robert fa tutte le sue cose quando si tratta di combattimenti.
+      Quando la telecamera è su di lui, è davvero lui a farlo".
+    </p>
+    <p>
+      Detto questo, Downey ha ammesso in un'intervista a Deadline dell'ottobre 2014 che stava iniziando ad allontanarsi
+      da quel lato del cinema mentre si avvicinava ai cinquant'anni, e stava lasciando che gli stuntman facessero il
+      loro lavoro, invece. "Sentendo lo schiocco delle mie ossa in Iron Man 3, sono finalmente uscito da quello che può
+      essere descritto solo come un volo di fantasia di cinque anni in cui ho pensato, se Sherlock Holmes può fare
+      questo, significa che posso farlo anch'io. Se Tony riesce a saltare da una parte all'altra di un'impalcatura e a
+      non rompersi la caviglia, beh, allora facciamolo due o tre volte e andiamo a pranzo. Guardando indietro, sono
+      sorpreso e davvero imbarazzato che un ragazzo di mezza età la pensasse così. E onestamente non la penso così al
+      momento", ha detto. "Tutti si fanno sempre male", ha aggiunto più tardi nell'intervista. "E poi dipende solo da
+      quale sia davvero la tua timoneria, qual è il compromesso e cosa sei disposto a fare. Sono ancora pronto a fare
+      ogni genere di cose. Penso che la lezione principale per me sia stata che ho momentaneamente smesso di prendere le
+      precauzioni che ho preso ogni altra volta, e ovviamente ciò che l'ha causato è stato un punto di vista psicologico
+      che avevo che non è realistico".
+    </p>
+
+    <p>
+      E Downey è ancora pronto a continuare la sua pratica fisica e spirituale con il Wing Chun. Continua anche a
+      rimanere pulito, concedendosi niente di più serio dell'occasionale antidolorifico post-allenamento. Quando lo
+      scrittore di Men's Health Stephen Rodrick lo ha raggiunto al dojo per un profilo nel maggio 2011, Downey ha dato
+      allo scrittore il seguente saggio consiglio: "Seriamente, non preoccuparti di sembrare un idiota. È come la vita:
+      meno sei consapevole di te stesso, meglio funziona. E ricorda, un sacco di ghiaccio e Advil dopo. Fidati di me su
+      questo".
+    </p>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-import videoPath from "../../assets/CORSI-2023-24.mp4"; // Import del video
-
-import imagePath from "../../assets/Eami1.jpg"; // Import dell'immagine
-const imageSrc = ref(imagePath);
-
-const isLightboxOpen = ref(false);
-const videoSrc = ref(videoPath);
-
-const openLightbox = () => {
-  isLightboxOpen.value = true;
-};
-
-const closeLightbox = () => {
-  isLightboxOpen.value = false;
+<script>
+export default {
+  name: "RobertDJunior",
 };
 </script>
-
-<style scoped>
-/* Nessuna modifica necessaria qui, solo assicurati che l'elemento principale abbia la struttura flex */
-</style>
